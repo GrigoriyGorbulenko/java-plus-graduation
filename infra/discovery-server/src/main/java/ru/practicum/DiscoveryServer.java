4cd8cbc70eb6b06a2634b67eceecf1a8c3b5a427
gorbulenkogv <gorbulenko.grigoriy@yandex.ru>
2025-07-07 01:33:56 +0300

    пересоздать класс GatewayServer

--- infra/discovery-server/src/main/java/ru/practicum/DiscoveryServer.java
+++ infra/discovery-server/src/main/java/ru/practicum/DiscoveryServer.java
@@ -1,7 +1,13 @@
 package ru.practicum;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServer  {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(DiscoveryServer .class, args);
     }
 }
